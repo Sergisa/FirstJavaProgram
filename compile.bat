fec03ac9813d672c171821e48d38b7bd6b0e4f88
Sergisa <phebra@yandex.ru>
2025-09-09 15:31:18 +0300

    пакетная сборка

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Java\jdk-21\bin\javac.exe" -d ./out/production/classes/ -cp ./src ./src/com/project/Main.java
+"C:\Program Files\Java\jdk-21\bin\javac.exe" -d ./out/production/classes/ -cp ./src ./src/com/project/*.java
 "C:\Program Files\Java\jdk-21\bin\java.exe" -cp ./out/production/classes com.project.Main
